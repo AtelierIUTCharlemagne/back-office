9d0f98f3bee2e36ab3565fb29b1ff4e9d2256442
Thibaud Turlan <45849917+ThibaudTurlan@users.noreply.github.com>
2022-03-30 15:52:33 +0200

    refactory: add route service admin

--- service_back_office/src/API/adminApi.js
+++ service_back_office/src/API/adminApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://docketu.iutnc.univ-lorraine.fr:62344/",
+  baseURL: "http://docketu.iutnc.univ-lorraine.fr:62344",
   headers: {
     "Content-type": "application/json"
   }
--- service_back_office/src/services/UsersDataService.js
+++ service_back_office/src/services/UsersDataService.js
@@ -1,14 +1,14 @@
-import http from "../API/userApi";
+import http from "../API/adminApi";
 
 class UsersDataService {
   getAll() {
-    return http.get("/users");
+    return http.get("/admin");
   }
   get(id) {
-    return http.get(`/users/${id}`);
+    return http.get(`/admin/${id}`);
   }
   delete(id) {
-    return http.delete(`/users/admin/delete/${id}`);
+    return http.delete(`/admin/delete/${id}`);
   }
 }
 export default new UsersDataService();
